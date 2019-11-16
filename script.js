21e86ab1e72dd23eb6881e2e32b09e02d587286d
Brittany <britloy91@gmail.com>
2019-11-16 14:12:11 -0500

    Finished html, started date update logic

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$("#currentDate").text(moment().format('dddd, MMMM Do'));
+
+
+function timeUpdate(){
+    $(".time").each(function(){
+    $("textarea").addClass("pastTime")
+    
+    
+    
+    })    
+    }
+    
+    timeUpdate()
+
+
+
+})
+

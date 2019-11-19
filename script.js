76f35b2773b16b73f85481470e6482a2392896c5
Brittany <britloy91@gmail.com>
2019-11-18 19:24:14 -0500

    poor attempt to fix hw

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
 $(document).ready(function () {
     $("#currentDate").text(moment().format('dddd, MMMM Do'));
 
-
+    // none of this shit is working man. NEED TO FIGURE OUT LOCAL STORAGE.
     function timeUpdate() {
         $(".time").each(function () {
             $("textarea").addClass("pastTime")
         })
-        $(".time").each(function() {
+        $(".time").each(function () {
             $("textarea").addClass("presentTime")
         })
-        $(".time").each(function() {
+        $(".time").each(function () {
             $("textarea").addClass("futureTime")
         })
 
 
     }
     timeUpdate()
+
+
+    var past = moment().isBefore();
+    var present = moment().isBetween();
+    var future = moment().isAfter();
+
+    //Determine how to get the isAfter to work???
+    function colorTimeBox(hour, html) {
+        var isBefore = moment(moment().format("hA"), "hA").isBefore(hour, "hA")
+        var isAfter = moment(moment().format("hA"), "hA").isAfter(hour, "hA")
+        //var isAfter = moment().isAfter(hour,"hour")
+
+        var isBetween = moment(moment().format("hA"), "hA").isSame(hour, "hA")
+
+        if (isAfter) {
+            html.style.backgroundColor = "grey"
+        }
+
+        if (isBefore) {
+            html.style.backgroundColor = "green"
+
+        }
+
+        if (isBetween) {
+            html.style.backgroundColor = "red"
+
+        }
+
+
+    }
+
+    var timeDivs = $(".time")
+
+    for (var i = 0; i < timeDivs.length; i++) {
+        var time = $(timeDivs[i]).children("p").text()
+        console.log(time)
+        var textarea = $(timeDivs[i]).children("textarea")
+        colorTimeBox(time, textarea[0])
+    }
+
+    //past time
+    //current time
+    //figure out how to activate save button
+    // $(document).ready(function () {
+    //     var saveButton = $('#button');
+    //     $(button).click(function () {
+    //         $(this).save();
+
+    //     })
+    // })
 })
-
-
-// var past = blocked Time();
-// var present = red Time();
-// var future = green Time();
-
-document.write('It\'s now: ', hours, '<br><br>');
-document.bgColor = "#808080";
-//determine how to correct this code to change the background color
-//past time
-if (past > 17 && hours < 20) {
-    document.write('<body style="background-color: grey">');
-}
-//current time
-else if (hours > 19 && hours < 22) {
-    document.write('<body style="background-color: red">');
-}
-//future time
-else if (hours > 21 || hours < 5) {
-    document.write('<body style="background-color: green;">');
-}

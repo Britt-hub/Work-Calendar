8fe3171aa06acf7b21e26651e9849fc8b52eab51
Brittany <britloy91@gmail.com>
2019-11-21 18:27:22 -0500

    final homewor

--- script.js
+++ script.js
@@ -1,58 +1,106 @@
 $(document).ready(function () {
     $("#currentDate").text(moment().format('dddd, MMMM Do'));
+       var currentTime = moment().format('H');
+       
 
     function timeUpdate() {
-        $(".time").each(function () {
-            $("textarea").addClass("pastTime")
-        })
-        $(".time").each(function () {
-            $("textarea").addClass("presentTime") 
-        })
-        $(".time").each(function () {
-            $("textarea").addClass("futureTime")
-        })
+
+if (currentTime < $('textarea[data-attribute="9"]').attr('data-attribute')){
+    ('textarea[data-attribute="9"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="9"]').attr('data-attribute')){
+    ('textarea[data-attribute="9"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="9"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="10"]').attr('data-attribute')){
+    ('textarea[data-attribute="10"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="10"]').attr('data-attribute')){
+    ('textarea[data-attribute="10"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="10"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="11"]').attr('data-attribute')){
+    ('textarea[data-attribute="11"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="11"]').attr('data-attribute')){
+    ('textarea[data-attribute="11"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="11"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="12"]').attr('data-attribute')){
+    ('textarea[data-attribute="12"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="12"]').attr('data-attribute')){
+    ('textarea[data-attribute="12"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="12"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="13"]').attr('data-attribute')){
+    ('textarea[data-attribute="13"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="13"]').attr('data-attribute')){
+    ('textarea[data-attribute="13"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="13"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="14"]').attr('data-attribute')){
+    ('textarea[data-attribute="14"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="14"]').attr('data-attribute')){
+    ('textarea[data-attribute="14"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="14"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="15"]').attr('data-attribute')){
+    ('textarea[data-attribute="15"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="15"]').attr('data-attribute')){
+    ('textarea[data-attribute="15"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="15"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="16"]').attr('data-attribute')){
+    ('textarea[data-attribute="16"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="16"]').attr('data-attribute')){
+    ('textarea[data-attribute="16"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="16"]').addClass("presentTime");
+}
+
+if (currentTime < $('textarea[data-attribute="17"]').attr('data-attribute')){
+    ('textarea[data-attribute="17"]').addClass("pastTime");
+} else if (currentTime > $('textarea[data-attribute="17"]').attr('data-attribute')){
+    ('textarea[data-attribute="17"]').addClass("futureTime");
+} else {
+    ('textarea[data-attribute="17"]').addClass("presentTime");
+}
+
+
+
+
+        // $(".time").each(function () {
+        //     $("textarea").addClass("pastTime")
+        // })
+        // $(".time").each(function () {
+        //     $("textarea").addClass("presentTime") 
+        // })
+        // $(".time").each(function () {
+        //     $("textarea").addClass("futureTime")
+        // })
 
 
     }
     timeUpdate()
 
-
-    var past = moment().isBefore();
-    var present = moment().isBetween();
-    var future = moment().isAfter();
-
-    //Determine how to get the isAfter to work???
-    function colorTimeBox(hour, html) {
-        var isBefore = moment(moment().format("hA"), "hA").isBefore(hour, "hA")
-        var isAfter = moment(moment().format("hA"), "hA").isAfter(hour, "hA")
-        //var isAfter = moment().isAfter(hour,"hour")
-
-        var isBetween = moment(moment().format("hA"), "hA").isSame(hour, "hA")
-
-        if (isAfter) {
-            html.style.backgroundColor = "grey"
-        }
-
-        if (isBefore) {
-            html.style.backgroundColor = "green"
-
-        }
-
-        if (isBetween) {
-            html.style.backgroundColor = "red"
-
-        }
-
-
-    }
-
     var timeDivs = $(".time")
 
     for (var i = 0; i < timeDivs.length; i++) {
         var time = $(timeDivs[i]).children("p").text()
-        console.log(time)
+        // console.log(time)
         var textarea = $(timeDivs[i]).children("textarea")
-        colorTimeBox(time, textarea[0])
+        // colorTimeBox(time, textarea[0])
     }
 
     //past time
